b2ce08527547d9bb02e3b77d71faf50b852ba732
khaled kedhai <khaled.kedhai@esprit.tn>
2022-04-19 02:53:18 +0200

    backOffice

---
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,20 @@
+
+import { Routes } from '@angular/router';
+
+import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
+// import { IconsComponent } from '../../pages/icons/icons.component';
+// import { MapsComponent } from '../../pages/maps/maps.component';
+
+import { VoyagesComponent } from '../../pages/Voyages/voyages.component';
+import { ListevoyageComponent } from 'src/app/pages/liste_voyage/listevoyage.component';
+import { VoyageseditComponent } from 'src/app/pages/Voyages-edit/voyagesedit.component';
+
+export const AdminLayoutRoutes: Routes = [
+    { path: 'dashboard',      component: DashboardComponent },
+ 
+{path:'voyages',component:VoyagesComponent},
+{path:'voyag/:id',component:VoyageseditComponent},
+{path:'liste',component:ListevoyageComponent},
+
+ ];
+ 
